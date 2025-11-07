e6d9d77080f3f5a9472cfdbf0c4d17a6c92f0870
jdelacruz0563 <jdelacruz0563@utm.edu.ec>
2025-11-07 09:19:47 -0500

    componete alquiler

---
+++ src/app/modules/user/alquiler/alquiler.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Alquiler } from './alquiler';
+
+describe('Alquiler', () => {
+  let component: Alquiler;
+  let fixture: ComponentFixture<Alquiler>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Alquiler]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Alquiler);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/user/alquiler/alquiler.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alquiler',
+  imports: [],
+  templateUrl: './alquiler.html',
+  styleUrl: './alquiler.scss',
+})
+export class Alquiler {
+
+}
